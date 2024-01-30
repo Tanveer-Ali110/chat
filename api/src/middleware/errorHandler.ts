b5c72384bba4adff8c655d6a1ffdaa388d137fa1
Tanveer-Ali110 <ali.tanveer1012@gmail.com>
2024-01-30 14:42:35 +0500

    fix: error handler

--- api/src/middleware/errorHandler.ts
+++ api/src/middleware/errorHandler.ts
@@ -1,10 +1,12 @@
-import { NextFunction, RequestHandler, Response } from "express";
+import { HttpException } from "@utils/exceptions";
+import { NextFunction, Response } from "express";
 
-export const errorHandler = (error: Error, req: Request, res: Response, next: NextFunction): Response => {
+export const errorHandler = (error: Error | HttpException, _: Request, res: Response, next: NextFunction): Response => {
   try {
+    const status = error instanceof HttpException ? error.HttpStatus : 500;
     const message: string = error.message || 'Something went wrong';
 
-    return res.status(500).json({ message });
+    return res.status(status).json({ message });
   } catch (error) {
     next(error);
   }
