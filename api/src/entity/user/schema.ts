2355f34a95487dfe184ab08c471d3c68f658310d
Tanveer-Ali110 <ali.tanveer1012@gmail.com>
2024-01-30 14:02:23 +0500

    fix: exception

--- api/src/entity/user/schema.ts
+++ api/src/entity/user/schema.ts
@@ -4,6 +4,7 @@ import { encryptPassword, generateAuthToken } from './trigger';
 import { head, isEmpty } from 'lodash';
 import { compare } from 'bcryptjs';
 import { IUser, IUserModel } from '@interfaces/user.interface';
+import { BadRequestException, UnAuthorizedException } from '@utils/exceptions';
 
 var validateEmail = function (email: string) {
     var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
@@ -36,23 +37,21 @@ export const UserSchema = new Schema<IUser, IUserModel>({
 
 UserSchema.pre("save", encryptPassword);
 
-// UserSchema.statics.findByCredentials = async (
-//     email: string,
-//     password: string
-// ) => {
-//     const users = await User.find({ email });
-//     if (isEmpty(users)) {
-//         throw new Error("User does not exist");
-//         //   throw new UnAuthorizedException("User does not exist");
-//     }
-//     const user: any = head(users);
-//     const isMatch = await compare(password, user.password);
-//     if (!isMatch) {
-//         throw new Error("Password does not match");
-//         //   throw new BadRequestException("Password does not match");
-//     }
-//     return user;
-// };
+UserSchema.statics.findByCredentials = async (
+    email: string,
+    password: string
+) => {
+    const users = await User.find({ email });
+    if (isEmpty(users)) {
+        throw new UnAuthorizedException("User does not exist");
+    }
+    const user: any = head(users);
+    const isMatch = await compare(password, user.password);
+    if (!isMatch) {
+        throw new BadRequestException("Password does not match");
+    }
+    return user;
+};
 
 // UserSchema.methods.generateAuthToken = generateAuthToken
 // UserSchema.set("toJSON", {
--- api/src/utils/errorHandler.ts
+++ api/src/utils/errorHandler.ts
@@ -1,11 +1,11 @@
 import { NextFunction, RequestHandler, Response } from "express";
 
-export const errorHandler = (error: Error, req: Request, res: Response, next: NextFunction):Response => {
-    try {
-        const message: string = error.message || 'Something went wrong';
-    
-        return res.status(500).json({ message });
-      } catch (error) {
-        next(error);
-      }
+export const errorHandler = (error: Error, req: Request, res: Response, next: NextFunction): Response => {
+  try {
+    const message: string = error.message || 'Something went wrong';
+
+    return res.status(500).json({ message });
+  } catch (error) {
+    next(error);
+  }
 }
